a6cf0ca7e91e0ad371c25ee47c4cab263ce66c3c
Billythecrescent <13550378056@163.com>
2021-03-25 22:14:21 +0100

    Separate codes into functions

--- task_1b/train.py
+++ task_1b/train.py
@@ -4,51 +4,96 @@ from sklearn.model_selection import KFold
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-
-data = pd.read_csv('datasets/train.csv', index_col=0).to_numpy()
+current_path = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(current_path,'datasets')
+data = pd.read_csv(os.path.join(data_path,'train.csv'), index_col=0).to_numpy()
 y = data[:, 0]
-X = data[:, 1:]
+X = data[:, 1:] #size = (700, 5)
 X = np.concatenate([X, np.square(X), np.exp(X), np.cos(X), np.ones(shape=(X.shape[0], 1))], axis=1)
-print('the size of featrues is: {}'.format(X.shape))
+print('the size of featrues is: {}'.format(X.shape)) #(700, 21)
 
-scores = []
-# alphas = [0.001, 0.1, 1, 10, 100]
-alphas = np.arange(10, 30, 1).tolist()
-weights = np.zeros(shape=(X.shape[1], len(alphas)))
-split = KFold(n_splits=10, random_state=0, shuffle=True)
+def BestAlphaList(times):
+    LeastScoreAlphas = np.zeros(shape = (times,1))
+    LeastScores = np.zeros(shape = (times,1))
+    for rnd in range(times):
+        scores = []
+        # alphas = [0.001, 0.1, 1, 10, 100]
+        alphas = np.arange(15, 20, 1).tolist()
+        weights = np.zeros(shape=(X.shape[1], len(alphas)))
+        split = KFold(n_splits=10, random_state=0, shuffle=True)
+        for i, alpha in enumerate(alphas):
+            each_score = []
+            each_coef_ = []
+            for train_index, test_index in split.split(X):
+                X_train, X_test = X[train_index], X[test_index]
+                y_train, y_test = y[train_index], y[test_index]
+                model = Ridge(alpha=alpha, solver='sag', fit_intercept=False).fit(X_train, y_train)
+                each_coef_.append(model.coef_)
+                y_pred = model.predict(X_test)
+                each_score.append(np.sqrt(np.mean(np.square(y_pred-y_test), axis=0)))
+            scores.append(np.mean(each_score))
+        least_score = min(scores)
+        LeastScores[rnd] = least_score
+        LeastScoreAlphas[rnd]  = alphas[scores.index(least_score)]
+    AlphaList = pd.DataFrame(np.concatenate([LeastScores,LeastScoreAlphas], axis=1), columns=['score','alpha'])
+    print(AlphaList)
+    return AlphaList
 
-plt.figure(figsize=(24, 8))
-plt.subplot(121)
+def FindBestAlpha(): 
+    AlphaList = BestAlphaList(1000)
+    print(np.mean(AlphaList['alpha']), np.mean(AlphaList['score']))
+    least_alphas = AlphaList['alpha'].tolist()
+    print({x:least_alphas.count(x) for x in least_alphas})
 
-for i, alpha in enumerate(alphas):
-    each_score = []
-    each_coef_ = []
-    for train_index, test_index in split.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        model = Ridge(alpha=alpha, solver='sag', fit_intercept=False).fit(X_train, y_train)
-        each_coef_.append(model.coef_)
-        y_pred = model.predict(X_test)
-        each_score.append(np.sqrt(np.mean(np.square(y_pred-y_test), axis=0)))
-    weights[:, i] = np.mean(np.asarray(each_coef_), axis=0)
-    plt.plot(np.arange(weights.shape[0]), weights[:, i], label='lambda_{:4f}'.format(alpha))
-    scores.append(np.mean(each_score))
+    plt.figure(figsize=(24, 8))
+    counts, bins = np.histogram(AlphaList['alpha'])
+    plt.hist(bins[:-1], bins, weights=counts)
+    plt.legend()
+    plt.show()
 
-plt.legend()
-plt.xlabel('weight index')
+def showAlphaEffect(Afrom, Ato):
+    scores = []
+    # alphas = [0.001, 0.1, 1, 10, 100]
+    alphas = np.arange(Afrom, Ato, 1).tolist()
+    weights = np.zeros(shape=(X.shape[1], len(alphas)))
+    split = KFold(n_splits=10, random_state=0, shuffle=True)
 
-plt.subplot(122)
-plt.plot(alphas, scores, label='scores')
-plt.legend()
-plt.xlabel('lambda')
+    plt.figure(figsize=(24, 8))
+    plt.subplot(121)
 
-plt.show()
+    for i, alpha in enumerate(alphas):
+        each_score = []
+        each_coef_ = []
+        for train_index, test_index in split.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            model = Ridge(alpha=alpha, solver='sag', fit_intercept=False).fit(X_train, y_train)
+            each_coef_.append(model.coef_)
+            y_pred = model.predict(X_test)
+            each_score.append(np.sqrt(np.mean(np.square(y_pred-y_test), axis=0)))
+        weights[:, i] = np.mean(np.asarray(each_coef_), axis=0)
+        plt.plot(np.arange(weights.shape[0]), weights[:, i], label='lambda_{:4f}'.format(alpha))
+        scores.append(np.mean(each_score))
 
-pd.DataFrame(data=np.asarray(scores)).to_csv('datasets/scores.csv', index=None, header=False)
-pd.DataFrame(data=np.asarray(weights)).to_csv('datasets/weights.csv', index=None, header=False)
+    plt.legend()
+    plt.xlabel('weight index')
 
-best_model = Ridge(alpha=300, fit_intercept=False).fit(X, y)
-pd.DataFrame(data=best_model.coef_).to_csv('datasets/submission1.csv', index=None, header=False)
-# best_model = Lasso(alpha=0.0035).fit(X, y)
-pd.DataFrame(data=np.asarray(np.mean(weights, axis=1))).to_csv('datasets/submission.csv', index=None, header=False)
+    plt.subplot(122)
+    plt.plot(alphas, scores, label='scores')
+    plt.legend()
+    plt.xlabel('lambda')
+
+    plt.show()
+
+    pd.DataFrame(data=np.asarray(scores)).to_csv(os.path.join(data_path,'scores.csv'), index=None, header=False)
+    pd.DataFrame(data=np.asarray(weights)).to_csv(os.path.join(data_path,'weights.csv'), index=None, header=False)
+
+def fitBestModel(alp):
+    best_model = Ridge(alpha=alp, fit_intercept=False).fit(X, y)
+    pd.DataFrame(data=best_model.coef_).to_csv(os.path.join(data_path,'submission1.csv'), index=None, header=False)
+
+# FindBestAlpha()
+# showAlphaEffect(10,300)
+fitBestModel(325)
